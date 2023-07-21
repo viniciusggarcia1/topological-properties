4fdd92b039d410370d2732cbc815c18523a0468d
viniciusggarcia1 <viniciusggarcia1@hotmail.com>
2023-07-20 23:13:14 -0300

    new version

--- main.py
+++ main.py
@@ -4,12 +4,83 @@
 # Federal University of Espírito Santo - UFES
 # viniciusggarcia1@hotmail.com
 
+import numpy as np
 import qsh
-import proj
+
+# Parâmetros da Hamiltoniana
+t = float(input('Enter the hopping parameter (t): '))
+M = float(input('Enter the topological mass (M): '))
+fixed_kx = 1
+fixed_ky = 2
+
+# Criar uma malha de pontos no espaço dos momentos
+num_points = 100
+kx_range = np.linspace(-np.pi, np.pi, num_points)
+ky_range = np.linspace(-np.pi, np.pi, num_points)
+phases = np.zeros((num_points, num_points))
+
+# Calcular a fase de Berry para cada ponto da malha
+for i, kx in enumerate(kx_range):
+    for j, ky in enumerate(ky_range):
+        phases[i, j] = qsh.berry_phase(kx, ky, t, M)
+
+# Plotar a estrutura de bandas
+num_bands = 2
+energies = np.zeros((num_points, num_bands))
+E0=[]
+E1=[]
+
+# Calcular as energias para cada ponto da malha
+for i, kx in enumerate(kx_range):
+    eigenvalues, _ = qsh.diagonalize_hamiltonian(kx, 0, t, M)  # Fixar ky em 0
+    #energies[i, :] = eigenvalues
+    E0.append(eigenvalues[0])
+    E1.append(eigenvalues[1])
+
+#Transição de Fase Kx fixo
+# Plotar a fase de Berry para kx fixo
+phases_fixed_kx = np.zeros(num_points)
+
+# Calcular a fase de Berry para kx fixo e ky variando
+for i, ky in enumerate(ky_range):
+    phases_fixed_kx[i] = qsh.berry_phase(fixed_kx, ky, t, M)
+
+#Transição de Fase Ky fixo
+# Plotar a fase de Berry para ky fixo
+phases_fixed_ky = np.zeros(num_points)
+
+# Calcular a fase de Berry para kx fixo e ky variando
+for i, kx in enumerate(kx_range):
+    phases_fixed_ky[i] = qsh.berry_phase(kx, fixed_ky, t, M)
+
+#Calculando a projeção em spin up e spin down
+sigma_z = np.array([[1, 0], [0, -1]])
+Vconj0=[]
+Vconj1=[]
+V0=[]
+V1=[]
+weight0=[]
+weight1=[]
+E0=[]
+E1=[]
+
+for kx in range (0, len(kx_range), 1):
+    ky=kx
+    E, V=qsh.diagonalize_hamiltonian(kx_range[kx], ky_range[kx], t, M)
+    E0.append(E[0])
+    E1.append(E[1])
+    V0.append(V[0])
+    V1.append(V[1])
+    Vconj0.append(np.conj(V[0]))
+    Vconj1.append(np.conj(V[1]))
+
+    weight0.append(Vconj0[kx][0]*(sigma_z[0][0]*V0[kx][0] + sigma_z[0][1]*V0[kx][1]) + Vconj0[kx][1]*(sigma_z[1][0]*V0[kx][0] + sigma_z[1][1]*V0[kx][1]))
+
+    weight1.append(Vconj1[kx][0]*(sigma_z[0][0]*V1[kx][0] + sigma_z[0][1]*V1[kx][1]) + Vconj1[kx][1]*(sigma_z[1][0]*V1[kx][0] + sigma_z[1][1]*V1[kx][1]))
 
 #Chamaremos todas as funções de plot para salvar os gráficos
-qsh.graph_berry()
-qsh.graph_bands()
-qsh.graph_kx_fixed()
-qsh.graph_ky_fixed()
-proj.graph_proj()
+qsh.graph_berry(phases, kx_range, ky_range)
+qsh.graph_bands(kx_range, E0, E1, M, t)
+qsh.graph_kx_fixed(ky_range, phases_fixed_kx, fixed_kx)
+qsh.graph_ky_fixed(kx_range, phases_fixed_ky, fixed_ky)
+qsh.graph_proj(weight0, weight1, kx_range, E0, E1, M, t)
--- qsh.py
+++ qsh.py
@@ -1,12 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# Parâmetros da Hamiltoniana
-t = float(input('Enter the hopping parameter (t): '))
-M = float(input('Enter the topological mass (M): '))
-fixed_kx = 1
-fixed_ky = 1
-
 # Definir a Hamiltoniana
 def hamiltonian(kx, ky, t, M):
     sigma_x = np.array([[0, 1], [1, 0]])
@@ -27,47 +21,20 @@ def berry_phase(kx, ky, t, M):
     phase = np.angle(np.prod(np.conj(eigenvectors[:, 0])))
     return phase
 
-# Criar uma malha de pontos no espaço dos momentos
-num_points = 100
-kx_range = np.linspace(-np.pi, np.pi, num_points)
-ky_range = np.linspace(-np.pi, np.pi, num_points)
-phases = np.zeros((num_points, num_points))
-
-# Calcular a fase de Berry para cada ponto da malha
-for i, kx in enumerate(kx_range):
-    for j, ky in enumerate(ky_range):
-        phases[i, j] = berry_phase(kx, ky, t, M)
-
 # Plotar a fase de Berry em função de Kx e Ky 
-
-def graph_berry():
+def graph_berry(phases, kx_range, ky_range):
     plt.figure()
     plt.imshow(phases.T, origin='lower', extent=[kx_range[0], kx_range[-1], ky_range[0], ky_range[-1]], cmap='jet')
     plt.colorbar(label='Berry Phase')
-
     plt.xlabel('K$_x$', fontsize=18)
     plt.ylabel('K$_y$', fontsize=18)
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
-    #plt.title('Berry Phase in Momentum Space')
     plt.tight_layout()
     plt.savefig('berry.png', dpi=800)
 
 # Plotar a estrutura de bandas
-num_bands = 2
-energies = np.zeros((num_points, num_bands))
-E0=[]
-E1=[]
-
-# Calcular as energias para cada ponto da malha
-for i, kx in enumerate(kx_range):
-    eigenvalues, _ = diagonalize_hamiltonian(kx, 0, t, M)  # Fixar ky em 0
-    #energies[i, :] = eigenvalues
-    E0.append(eigenvalues[0])
-    E1.append(eigenvalues[1])
-
-# Plotar a estrutura de bandas
-def graph_bands():
+def graph_bands(kx_range, E0, E1, M, t):
     plt.figure()
     plt.plot(kx_range, E0, c='blue', label=f'M={M} and t={t}')
     plt.plot(kx_range, E1, c='blue')
@@ -79,19 +46,8 @@ def graph_bands():
     plt.tight_layout()
     plt.savefig('bands.png', dpi=800)
 
-
-#Transição de Fase Kx fixo+++++++++++++++++++++++++++++++++++++++++++++++++
-
 # Plotar a fase de Berry para kx fixo
-
-phases_fixed_kx = np.zeros(num_points)
-
-# Calcular a fase de Berry para kx fixo e ky variando
-for i, ky in enumerate(ky_range):
-    phases_fixed_kx[i] = berry_phase(fixed_kx, ky, t, M)
-
-# Plotar a fase de Berry para kx fixo
-def graph_kx_fixed():
+def graph_kx_fixed(ky_range, phases_fixed_kx, fixed_kx):
     plt.figure()
     plt.plot(ky_range, phases_fixed_kx, color='r', label=f'k$_x$={fixed_kx}')
     plt.xlabel('K$_y$', fontsize=18)
@@ -102,18 +58,8 @@ def graph_kx_fixed():
     plt.tight_layout()
     plt.savefig('kx_fixed.png', dpi=800)
 
-#Transição de Fase Ky fixo+++++++++++++++++++++++++++++++++++++++++++++++++
-
 # Plotar a fase de Berry para ky fixo
-
-phases_fixed_ky = np.zeros(num_points)
-
-# Calcular a fase de Berry para kx fixo e ky variando
-for i, kx in enumerate(kx_range):
-    phases_fixed_ky[i] = berry_phase(kx, fixed_ky, t, M)
-
-# Plotar a fase de Berry para ky fixo
-def graph_ky_fixed():
+def graph_ky_fixed(kx_range, phases_fixed_ky, fixed_ky):
     plt.figure()
     plt.plot(kx_range, phases_fixed_ky, color='r', label=f'k$_y$={fixed_ky}')
     plt.xlabel('K$_x$', fontsize=18)
@@ -123,3 +69,19 @@ def graph_ky_fixed():
     plt.legend()
     plt.tight_layout()
     plt.savefig('ky_fixed.png', dpi=800)
+
+#Função para calcular a projeção
+def graph_proj(weight0, weight1, kx_range, E0, E1, M, t):
+    plt.figure()
+    for i in range (0, len(E0), 1):
+        weight0[i]=weight0[i].real
+        weight1[i]=weight1[i].real
+        plt.scatter(kx_range[i], E0[i], s=10, c=5*weight0[i], vmin=-1, vmax=1, cmap='bwr')
+        plt.scatter(kx_range[i], E1[i], s=10, c=5*weight1[i], vmin=-1, vmax=1, cmap='bwr')
+    
+    plt.ylabel('Energy', fontsize=18)
+    plt.yticks(fontsize=14)
+    plt.xlabel('k$_x$', fontsize=16)
+    plt.text(-0.7, 7, f'M={M} and t={t}')
+    plt.tight_layout()
+    plt.savefig('proj.png', dpi=800)
